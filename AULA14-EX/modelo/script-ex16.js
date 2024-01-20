de9b24b197b3ad03cf91f851d53dc626d11e5466
Jônatas Aragão <aragaojonatas280@gmail.com>
2024-01-20 18:46:23 -0300

    mais uma atualização

--- AULA14-EX/modelo/script-ex16.js
+++ AULA14-EX/modelo/script-ex16.js
@@ -5,16 +5,34 @@ function contar(){
     var res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
-        window.alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossível contar'
     } else{
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>'
         var inicio = Number(inicio.value)
         var fim= Number(fim.value)
         var passo = Number(passo.value)
 
-        for(var c = inicio; c <= fim; c += passo){
-            res.innerHTML += ` ${c}  `
+
+        if(passo <= 0){
+            window.alert('Passo inválido!')
+        }
+        
+        if( inicio < fim){
+            //contagem crescente
+             for(var c = inicio; c <= fim; c += passo){
+            res.innerHTML += ` ${c} \u{1F449} `
+            }
+
+        }else{
+            //Contagem regressiva
+            for(var c  = inicio; c >= fim; c -= passo){
+                res.innerHTML += ` ${c} \u{1F449} `
+                
+            }
 
         }
+
+        res.innerHTML += `\u{1f3c1}`
+       
     }
 }
