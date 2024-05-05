6114dc092a6859456fedd770bddd486c993ea311
Jônatas Aragão <aragaojonatas280@gmail.com>
2024-05-04 23:51:07 -0300

    condições-estudos

---
+++ aulas/modulo-D/condição-composta.js
@@ -0,0 +1,7 @@
+let pais = 'EUA'
+console.log(`Vivendo em ${pais}` )
+if(pais == 'BRASIL'){
+    console.log('Brasileiro')
+}else{
+    console.log('você é estrangeiro')
+}
---
+++ aulas/modulo-D/condição-simples.js
@@ -0,0 +1,6 @@
+// Testando condicionais
+let vel = 12
+// condição somples
+if(vel >= 80){
+    console.log(`A velocidade do seu carro é ${vel},Você ultrapassou a velocidade permitida`)
+}
